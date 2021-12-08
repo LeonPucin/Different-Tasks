7b2492af5fdca06635b60b9557a22b07d403526d
Pucin <77212731+LeonPucin@users.noreply.github.com>
2021-12-09 01:08:45 +0300

    Add files via upload

---
+++ Tasks/BilliardsTask.cs
@@ -0,0 +1,17 @@
+﻿namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">The angle of the direction of movement of the ball</param>
+        /// <param name="wallInclinationRadians">Angle from the X axis</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            //TODO
+            return (wallInclinationRadians - directionRadians) + wallInclinationRadians;
+        }
+    }
+}
